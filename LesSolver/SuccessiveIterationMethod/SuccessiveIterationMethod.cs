4dd99ddf22d9acb8b5aa3efb2be33d32c66bd1cb
Stepan Sapozhkov <sapozhkov.stepan@saritasa.com>
2020-09-14 22:37:15 +0700

    Добавил условие сходимости, наверное даже будет работать.

--- LesSolver/SuccessiveIterationMethod/SuccessiveIterationMethod.cs
+++ LesSolver/SuccessiveIterationMethod/SuccessiveIterationMethod.cs
@@ -1,10 +1,43 @@
-﻿namespace LesSolver.SuccessiveIterationMethod
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+
+namespace LesSolver.SuccessiveIterationMethod
 {
     public class SuccessiveIterationMethod : ISolutionMethod
     {
         public double[] Solve(double[,] matrixA, double[] matrixB, double delta)
         {
-            throw new System.NotImplementedException();
+            var leftMatrix = DenseMatrix.OfArray(matrixA);
+            var rightMatrix = DenseVector.OfArray(matrixB);
+
+            if (!CheckConvergence(leftMatrix))
+            {
+                throw new InvalidOperationException("Условие сходимости не выполнилось");
+            }
+
+            //TODO
+            return null;
+        }
+
+        private bool CheckConvergence(Matrix<double> leftMatrix)
+        {
+            for (int i = 0; i < leftMatrix.RowCount; i++)
+            {
+                double sum = 0;
+                double mainElement = 0;
+                for (int j = 0; j < leftMatrix.ColumnCount; j++)
+                {
+                    if (i == j)
+                        mainElement = leftMatrix[i, j];
+                    else
+                        sum += Math.Abs(leftMatrix[i, j]);
+
+                }
+                if (Math.Abs(mainElement) <= sum)
+                    return false;
+            }
+            return true;
         }
     }
 }
