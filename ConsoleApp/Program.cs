540b7d85b4144204d213632d217624d0e2f480d5
Stepan Sapozhkov <sapozhkov.stepan@saritasa.com>
2020-09-21 19:52:02 +0700

    Добавил решалу для этого метода, должно работать.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using LesSolver.GaussMethod;
+﻿using LesSolver.SuccessiveIterationMethod;
+using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp
 {
@@ -9,10 +11,17 @@ namespace ConsoleApp
             // Это пока не работает, просто пример использования.
             var solver = new LesSolver.LesSolver
             {
-                SolutionMethod = new GaussSolutionMethod()
+                SolutionMethod = new SuccessiveIterationMethod()
             };
 
-            solver.Solve(new double[5, 5], new double[5], 0.5);
+            double[] solutions = solver.Solve(new double[4, 4] { {7, 0.9, 0.5, 0.6 },
+                                                                 { 0.2, 12, 1.3, 1.1 },
+                                                                 { 1.2, 0.9, 6.5, 0.4 },
+                                                                 { 1, 0.5, 0.9, 10 }
+                                                                }, new double [4] { 7, 11, -9, 8 }, 0.001);
+            foreach (var s in solutions)
+                Console.WriteLine(s);
+            Console.ReadKey();
         }
     }
 }
--- LesSolver/SuccessiveIterationMethod/SuccessiveIterationMethod.cs
+++ LesSolver/SuccessiveIterationMethod/SuccessiveIterationMethod.cs
@@ -16,8 +16,7 @@ namespace LesSolver.SuccessiveIterationMethod
                 throw new InvalidOperationException("Условие сходимости не выполнилось");
             }
 
-            //TODO
-            return null;
+            return FindSolution(leftMatrix, rightMatrix, delta);
         }
 
         private bool CheckConvergence(Matrix<double> leftMatrix)
@@ -38,6 +37,52 @@ namespace LesSolver.SuccessiveIterationMethod
                     return false;
             }
             return true;
+        }
+
+        private double[] FindSolution(Matrix<double> leftMatrix, DenseVector rightMatrix, double delta)
+        {
+            bool isRightSolution = false;
+            bool isFirstIteration = true;
+            double[] solutions = new double[leftMatrix.ColumnCount];
+            double[] solutionsTemp = new double[leftMatrix.ColumnCount];
+
+            //Make sure that everything is set to zero 
+            for (int i = 0; i < solutions.Length; i++)
+                solutions[i] = 0;
+
+            while (!isRightSolution)
+            {
+                for (int i = 0; i < leftMatrix.RowCount; i++)
+                {
+                    double sum = 0;
+                    double divider = 0;
+                    for (int j = 0; j < leftMatrix.ColumnCount; j++)
+                    {
+                        if (i != j)
+                            sum -= solutions[j] * leftMatrix[i, j];
+                        else
+                            divider = leftMatrix[i, j];
+                    }
+                    solutionsTemp[i] = (sum + rightMatrix[i]) / divider;
+                }
+
+                if (isFirstIteration)
+                    isFirstIteration = false;
+                else
+                {
+                    double max = double.MaxValue;
+                    for (int i = 0; i < solutionsTemp.Length; i++)
+                    {
+                        if (max > Math.Abs(solutionsTemp[i] - solutions[i]))
+                            max = Math.Abs(solutionsTemp[i] - solutions[i]);
+                        solutions[i] = solutionsTemp[i];
+                    }
+
+                    if (delta > max)
+                        break;
+                }
+            }
+            return solutions;
         }
     }
 }
